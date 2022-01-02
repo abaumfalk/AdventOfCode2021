b30d510aa2f89199c908cede94d133e20af7e1c6
Arno Baumfalk <a.baumfalk@web.de>
2022-01-02 11:37:28 +0100

    added day07

---
+++ day07/day07_1.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+class CrabOptimizer:
+    def __init__(self, crabs):
+        self.crabs = crabs
+        self.opt_fuel, self.opt_pos = None, None
+
+    def optimize(self):
+        start = min(self.crabs)
+        end = max(self.crabs)
+        self.opt_pos, self.opt_fuel = start, self.get_fuel(start)
+
+        for pos in range(start + 1, end + 1):
+            fuel = self.get_fuel(pos)
+            if fuel < self.opt_fuel:
+                self.opt_pos, self.opt_fuel = pos, fuel
+
+        return self.opt_pos, self.opt_fuel
+
+    def get_fuel(self, pos):
+        return sum(abs(pos - c) for c in self.crabs)
+
+
+if __name__ == "__main__":
+    lines = Path('input').read_text().splitlines()
+    crab_positions = list(map(int, lines[0].split(',')))
+
+    crab_optimizer = CrabOptimizer(crab_positions)
+    p, f = crab_optimizer.optimize()
+
+    print(f"Crabs should align at {p}, using {f} fuel")
+
